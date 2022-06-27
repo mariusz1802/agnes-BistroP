04da06e1d2b33ae8f06edf3f40c56a31d66b7795
Mariusz <mariusz.janowski@yahoo.pl>
2022-06-27 13:10:16 +0200

    slider

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,32 @@
 module.exports = {
   siteMetadata: {
     title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "agnes-bistro"
-    }
-  }, "gatsby-plugin-sitemap"]
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `backgrounds`,
+        path: `${__dirname}/src/bg`, // wherever background images are stored
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "agnes-bistro",
+      },
+    },
+    "gatsby-plugin-sitemap",
+  ],
 };
---
+++ src/components/Hero.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import HeroSlider from "./HeroSlider";
+
+function Hero() {
+  return (
+    <>
+      <HeroContainer>
+        <HeroSlider />
+      </HeroContainer>
+    </>
+  );
+}
+
+export default Hero;
+
+const HeroContainer = styled.div`
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  justify-items: center;
+  width: 90%;
+  justify-items: center;
+`;
