890bdc161e6016c1f11779cc308ae85d2947c89a
Mariusz <mariusz.janowski@yahoo.pl>
2022-07-04 05:37:54 +0200

    newer version

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,26 @@
-import * as React from "react";
-import Hero from "../components/Hero";
-import Layout from "../components/Layout";
-import ScrollUpBtn from "../components/ScrollUpBtn/ScrollUpBtn";
-import AboutMe from "../components/AboutMe";
-import Realizations from "../components/Realizations";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
+import * as React from 'react';
+import Hero from '../components/Hero';
+import Layout from '../components/Layout';
+import ScrollUpBtn from '../components/ScrollUpBtn/ScrollUpBtn';
+import AboutMe from '../components/AboutMe';
+import Realizations from '../components/Realizations';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+import NoSSR from 'react-no-ssr';
 
 // markup
 const IndexPage = () => {
   return (
-    <Layout>
-      <Hero />
-      <ScrollUpBtn />
-      <AboutMe />
-      <Realizations />
-      <Contact />
-      <Footer />
-    </Layout>
+    <NoSSR>
+      <Layout>
+        <Hero />
+        <ScrollUpBtn />
+        <AboutMe />
+        <Realizations />
+        <Contact />
+        <Footer />
+      </Layout>
+    </NoSSR>
   );
 };
 
